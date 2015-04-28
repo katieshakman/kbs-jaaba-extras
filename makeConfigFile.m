fb71d26618f4e4e83550c0656f387ffd5e308702
katieshakman <katieshak@gmail.com>
2015-04-28 14:20:06 -0400

    Updates mainly to how config file is made.  Also commented out code to make directories in MyJAABAPrep that I didn't want to use any time soon.

--- makeConfigFile.m
+++ makeConfigFile.m
@@ -1,21 +1,19 @@
 %configure
 clear all; 
 expt = {}; 
-for idx = 1:10
-    num = 2621+idx; 
+list = [1];
+for idx = 1:length(list)
+    num = 2705+list(idx); 
     expt{idx} = num2str(num); 
 end
 % expt = {
 %     '2622';
 %     '2623';
 %     '2624';
-%     '2625';
-%     '2626';
-%     '2627';
-%     '2628';
 %     }; % cell array of strings containing expt IDs
+% Insert parent directory, ending with a /. 
 parentDir = ...
-    '/Volumes/KATIELAB1/Behavior_Videos_JAABA/First_10_Behavior_Movies/';
+    '/Volumes/KATIELAB1/Behavior_Videos_JAABA/Rest of Old Behavior Videos/';
 newExptDir = ...
     strcat(parentDir, expt);
 save('configFile'); 
